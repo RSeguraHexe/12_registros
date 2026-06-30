b8be07ec410b2114b0530a9f8bb23badc2ecfa4d
RSeguraHexe <rsegurah@unjbg.edu.pe>
2026-06-30 18:47:03 -0500

    se cambio el formato de 'mayor' a flotante; ahora ya se muestra el menor sueldo; se añadio la biblioteca <string>

--- Empleados.cpp
+++ Empleados.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 
 struct Empleado{
     std::string name;
@@ -7,33 +8,45 @@ struct Empleado{
 };
 
 int main(){
-    int n;
+    int n; system("cls");
     Empleado empleados[100];
     std::cout<<"Ingrese la cantidad de empleados: "; std::cin>>n;
     std::cin.ignore();
     
     for(int i=0; i<n; i++){
+        
         std::cout<<"=====================================================================\n";
         std::cout<<"Nombre del empleado "<<i+1<<": "; std::getline(std::cin, empleados[i].name);
         std::cout<<"Genero del empleado [M/F] "<<i+1<<": "; std::cin>>empleados[i].sexo; 
         std::cout<<"Sueldo del empleado "<<i+1<<": "; std::cin>>empleados[i].sueldo;
-        std::cout<<"=====================================================================\n";
+
         std::cin.ignore();
     }
-    int mayor=0, indice;
+
+    float mayor=0; int indicemayor=0;
     for(int i=0; i<n; i++){
         if(empleados[i].sueldo>mayor){
             mayor=empleados[i].sueldo;
-            indice=i;
+            indicemayor=i;
         }
     }
-    std::cout<<"Nombre del empleado con mayor sueldo: "<<empleados[indice].name<<"\n"; 
-    std::cout<<"Genero de empleado con mayor sueldo: "<<empleados[indice].sexo<<"\n";
-    std::cout<<"Sueldo del empleado con mayor sueldo: "<<empleados[indice].sueldo<<"\n";
+    std::cout<<"=====================================================================\n";
+    std::cout<<"Nombre del empleado con mayor sueldo: "<<empleados[indicemayor].name<<"\n"; 
+    std::cout<<"Genero de empleado con mayor sueldo: "<<empleados[indicemayor].sexo<<"\n";
+    std::cout<<"Sueldo del empleado con mayor sueldo: "<<empleados[indicemayor].sueldo<<"\n";
     std::cout<<"=====================================================================\n";
 
+    float menor=mayor; int indicemenor=0;
+    for(int i=0; i<n; i++){
+        if(empleados[i].sueldo<menor){
+            menor=empleados[i].sueldo;
+            indicemenor=i;
+        }
+    }
+    std::cout<<"Nombre del empleado con menor sueldo: "<<empleados[indicemenor].name<<"\n"; 
+    std::cout<<"Genero de empleado con menor sueldo: "<<empleados[indicemenor].sexo<<"\n";
+    std::cout<<"Sueldo del empleado con menor sueldo: "<<empleados[indicemenor].sueldo<<"\n";
+    std::cout<<"=====================================================================\n";
 
-
-    std::cout<<"Prueba de modificacion git 01";
     return 0;
 }
